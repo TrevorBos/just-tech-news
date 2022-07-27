fee70c1b1f42f8133be3160d5f0803149d89a46d
Trevor Bos <trevor.bos@outlook.com>
2022-07-27 07:36:11 -0400

    Added in comments ability, my code wasnt working properly, so I merged with school.

--- models/index.js
+++ models/index.js
@@ -1,26 +1,28 @@
-const User = require("./User");
-const Post = require("./post");
-const Vote = require("./vote");
+// import all models
+const Post = require('./Post');
+const User = require('./User');
+const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 // create associations
 User.hasMany(Post, {
-  foreignKey: "user_id",
+  foreignKey: 'user_id'
 });
 
 Post.belongsTo(User, {
-  foreignKey: "user_id",
+  foreignKey: 'user_id'
 });
 
 User.belongsToMany(Post, {
   through: Vote,
-  as: "voted_posts",
-  foreignKey: "user_id",
+  as: 'voted_posts',
+  foreignKey: 'user_id'
 });
 
 Post.belongsToMany(User, {
   through: Vote,
-  as: "voted_posts",
-  foreignKey: "post_id",
+  as: 'voted_posts',
+  foreignKey: 'post_id'
 });
 
 Vote.belongsTo(User, {
@@ -39,4 +41,20 @@ Post.hasMany(Vote, {
   foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
